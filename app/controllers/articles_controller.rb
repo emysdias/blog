d27a5798d145a791540c52350aa65495ea7d153c
Emily Sousa <diassousa@yahoo.com.br>
2021-06-09 00:22:51 -0300

    finish tutorial documentation, add delete methods to comments

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,15 @@
 class ArticlesController < ApplicationController
-  def new
-    @article = Article.new
-  end
-  
+
+  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+
   def index
     @articles = Article.all
+  end
+
+  # snippet for brevity
+
+  def new
+    @article = Article.new
   end
   
   def show
